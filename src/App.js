ee81bf8fe6282acbf31774fd5d6354698d954e86
David steinhafel <davidsteinhafel@outlook.com>
2021-01-28 16:09:38 -0600

    created react app. changed default photo to myself and made it a clickable link taking user to my linkedin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import logo from './dave.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a href="https://www.linkedin.com"><img src= {logo} className="App-logo" alt="logo" /></a>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello world
         </a>
       </header>
     </div>
