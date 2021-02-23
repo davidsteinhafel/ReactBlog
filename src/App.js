f2abc0aba33350118e1470ae23fe3f27913a12fc
David steinhafel <davidsteinhafel@outlook.com>
2021-02-22 18:38:23 -0600

    more adjusting of the taskbar styling. removed the background picture of the footer and just found a similar solid color. Added first additions of desktop icons to the site and placed in a table. Table allowed for easier positioning.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./img/dave_talk.gif";
 import start from "./img/start.png";
+import folder from "./img/folder_95.png";
+import computer from "./img/comp_95.png";
 import "./App.css";
 import "98.css";
 import Clock from "./Clock";
@@ -10,27 +12,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://www.linkedin.com">
+        {/* <a href="https://www.linkedin.com">
           <img src={logo} className="App-logo" alt="logo" />
-        </a>
-        <p>
-          David Steinhafel's First React Portfolio/Blog Website with a window's
-          95 theme
-        </p>
+        </a> */}
         <a
           className="App-link"
           href="https://www.linkedin.com"
           target="_blank"
           rel="noopener noreferrer"
         ></a>
+        <table className="App-table">
+          <tr>
+            <td>
+              <img src={folder} className="App-folder" alt="folder"></img>
+            </td>
+            <td>
+              <img src={computer} className="App-computer" alt="computer"></img>
+            </td>
+          </tr>
+        </table>
       </header>
+
       <footer className="App-footer">
+        {/* possible fix might be to put both objects into table  */}
         <a href="https://www.google.com">
           <img src={start} className="App-start" alt="start" />
         </a>
-        <span>
-          <Clock></Clock>
-        </span>
+        <Clock></Clock>
       </footer>
     </div>
   );
